30ea631dd538bb6edcb5a226cb53242e54c714cd
ayankhairollayev <ayankhairlollayev@gmail.com>
2025-03-03 04:22:54 +0500

    added mustache template for video player

--- src/main/java/kz/khairollayev/videoplatformbackend/controller/VideoController.java
+++ src/main/java/kz/khairollayev/videoplatformbackend/controller/VideoController.java
@@ -3,10 +3,12 @@ package kz.khairollayev.videoplatformbackend.controller;
 import kz.khairollayev.videoplatformbackend.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 @RequestMapping("/videos")
 @RequiredArgsConstructor
 public class VideoController {
@@ -22,5 +24,11 @@ public class VideoController {
                                               @RequestHeader(value = "Range", required = false) String rangeHeader) {
 
         return videoService.streamVideo(id, rangeHeader);
+    }
+
+    @GetMapping("/watch")
+    public String watchVideo(@RequestParam Long id, Model model) {
+        model.addAttribute("id", id);
+        return "video";
     }
 }
